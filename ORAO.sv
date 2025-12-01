3378ec19e90eee505bef1a0d8c174b60b76b7122
jackyangantelope <jack@retroremake.co>
2025-12-01 14:56:58 +0800

    modify_frame

--- ORAO.sv
+++ ORAO.sv
@@ -59,6 +59,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -188,6 +190,8 @@ assign VGA_F1 = 0;
 assign VGA_SCALER  = 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 assign AUDIO_MIX = 0;
 
